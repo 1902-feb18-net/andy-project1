444e6071eb69f3da963ebc5057e0d3f782d2e755
Andy Ly <andywindsong@gmail.com>
2019-03-11 07:20:03 -0500

    time is mostly up at this point in time

--- ClothingStore/ClothingStore.Testing/UnitTest1.cs
+++ ClothingStore/ClothingStore.Testing/UnitTest1.cs
@@ -1,5 +1,11 @@
 using System;
 using Xunit;
+using Microsoft.AspNetCore.Mvc;
+using ClothingStore.WebApp.Controllers;
+using ClothingStore.WebApp.Models;
+using ClothingStore.Lib;
+using System.Collections.Generic;
+using Moq;
 
 namespace ClothingStore.Testing
 {
@@ -9,6 +15,15 @@ namespace ClothingStore.Testing
         public void Test1()
         {
 
+        }
+
+        public void GetCreateReturnsCustomerView()
+        {
+            // arrange
+            var customers = new List<Lib.Customer> { new Lib.Customer { Id = 1, DefaultStoreId = 1, FirstName = "Bob", LastName = "Lee" } };
+            var mockRepo = new Mock<ICustomerRepo>();
+            mockRepo.Setup(c => c.GetCustomers()).Returns(customers);
+            //var sut = new CustomersController(mockRepo.Object);
         }
     }
 }
